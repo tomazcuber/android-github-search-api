0b3adae4789712e25b33578bbd11d08d576da0b0
tomazcuber <tomaz_br_23@hotmail.com>
2024-06-27 19:20:44 -0300

    Model response from list repos and repo tags

--- GithubApi/src/main/java/br/com/githubapi/data/model/RepositoryTag.kt
+++ GithubApi/src/main/java/br/com/githubapi/data/model/RepositoryTag.kt
@@ -1,6 +1,19 @@
 package br.com.githubapi.data.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RepositoryTag(
-    val name: String,
-    val nodeId: String,
+    @SerialName("name") val name: String,
+    @SerialName("commit") val commit: Commit,
+    @SerialName("zipball_url") val zipballUrl: String,
+    @SerialName("tarball_url") val tarballUrl: String,
+    @SerialName("node_id") val nodeId: String
+)
+
+@Serializable
+data class Commit(
+    @SerialName("sha") val sha: String,
+    @SerialName("url") val url: String
 )
