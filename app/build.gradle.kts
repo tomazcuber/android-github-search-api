4f8ae71e930f492668730a6185de95c0190c97f1
Tomaz Cuber <tomazcuber@outlook.com>
2024-07-10 10:44:43 -0300

    Fix compilation error from build.gradle naming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "br.com.br.com.tomazcuber.githubsearch"
+        applicationId = "br.com.tomazcuber.githubsearch"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
